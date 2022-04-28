124314fca1665ef8a985cef468a93a5d74fb7e9c
Tony <Tony>
2022-04-28 23:44:48 +0800

    set default settings

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const toggleKeyLight = require('./keylight');
 const bonjour = require('bonjour')();
 
+const optionsWeb = {"On": 1, "Temperature": 226, "Brightness": 30};
+const optionsWhite = {"On": 1, "Temperature": 100, "Brightness": 80};
+
+let args = process.argv.slice(2).map(a => a.toLowerCase());
+let option = args.some(a => a === 'white') ? optionsWhite : args.some(a => a === 'web') ? optionsWeb : undefined;
+
 const browser = bonjour.find({type: 'elg'}, function (service) {
     toggleKeyLight({
-      ip: service['referer'].address,
-      port: service.port
-    });
+        ip: service['referer'].address,
+        port: service.port
+    }, option);
 });
 
 const counter = function (value = 6) {
@@ -19,6 +25,6 @@ const counter = function (value = 6) {
         bonjour.destroy();
     }
 }
-counter(3);
+counter(5);
 
 
--- keylight.js
+++ keylight.js
@@ -1,31 +1,39 @@
 const axios = require('axios').default;
 
 async function getKeylight(url) {
-  return await axios.get(url);
+    return await axios.get(url);
 }
 
-async function toggleKeyLight(keylightService) {
-  const keylightUrl = `http://${keylightService.ip}:${keylightService.port}/elgato/lights`;
-  const keylightData = await getKeylight(keylightUrl);
-  const light = keylightData.data.lights[0];
+async function toggleKeyLight(keylightService, option) {
+    const keylightUrl = `http://${keylightService.ip}:${keylightService.port}/elgato/lights`;
+    const keylightData = await getKeylight(keylightUrl);
+    console.log(`… `, keylightUrl, keylightData.data);
+    const light = keylightData.data.lights[0];
 
-  return light.on ? await turnLightOff(keylightUrl) : await turnLightOn(keylightUrl);
+    return light.on ? await turnLightOff(keylightUrl) : await turnLightOn(keylightUrl, option);
 }
 
 async function turnLightOff(url) {
-  return await axios.put(url, { "Lights": [
-      {
-        "On": 0
-      }
-    ]});
+    return await axios.put(url, {
+        "Lights": [
+            {
+                "On": 0
+            }
+        ]
+    });
 }
 
-async function turnLightOn(url) {
-  return await axios.put(url, { "Lights": [
-      {
-        "On": 1
-      }
-    ]});
+async function turnLightOn(url, options) {
+    return await axios.put(url, {
+        "Lights": [
+            {
+                ...{
+                    "On": 1
+                },
+                ...options,
+            }
+        ]
+    });
 }
 
 module.exports = toggleKeyLight;
