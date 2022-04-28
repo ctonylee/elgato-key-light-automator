005e83a474b7860eec709ae882369751395e6fed
Tony <Tony>
2022-04-28 22:59:31 +0800

    fix - toggle all services found and terminate program in 3 seconds

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const toggleKeyLight = require('./keylight');
 const bonjour = require('bonjour')();
 
-const browser = bonjour.find({ type: 'elg' });
-browser.on('up', (service) => {
-  toggleKeyLight({
-    ip: service['referer'].address,
-    port: service.port
-  });
-  bonjour.destroy();
+const browser = bonjour.find({type: 'elg'}, function (service) {
+    toggleKeyLight({
+      ip: service['referer'].address,
+      port: service.port
+    });
 });
+
+const counter = function (value = 6) {
+    console.log(`… terminating program in ${value--} second(s)`);
+    if (value > 0) {
+        setTimeout(() => {
+            counter(value)
+        }, 1000);
+
+    } else {
+        bonjour.destroy();
+    }
+}
+counter(3);
+
+
